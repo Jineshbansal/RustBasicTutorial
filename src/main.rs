1b6deb4a6a2834c46e1f1eaf6c6d30308299713f
Jineshbansal <732005jinesh@gmail.com>
2024-12-11 18:55:12 +0530

    Get string size

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,10 @@
 fn main() {
     println!("Hello, world!");
-    let num: i32= 5;
-    let result: i32 = fibo(num);
-    println!("Fibonacci of {} is {}", num, result);
+    let s = String::from("JineshBansal");
+    println!("The size of the string is: {}", get_string_size(&s));
 }
 
-fn fibo(num:i32)->i32{
-    let mut first=0;
-    let mut second=1;
-    if num==0{
-        return first;
-    }
-    if num==1{
-        return second;
-    }
-    for _ in 1..num{
-        let temp=second;
-        second=first+second;
-        first=temp;
-    }
-    return second;
+fn get_string_size(s: &String)-> usize{
+    s.chars().count()
 }
+ 
