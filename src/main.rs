9f247ad74f9cee21d27993d7f03ab6922efb6978
Jineshbansal <732005jinesh@gmail.com>
2024-12-11 18:33:41 +0530

    even function completed

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ fn main() {
     println!("{}",is_even(2));
 
 }
-
+// General practice in rust to write in snake case
 fn is_even(n:i32)->bool{
-    if n%2==0 {
+    if  n%2==0 {
         return true;
     }
     else{
