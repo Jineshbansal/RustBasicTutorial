1aebd9ab35eaabf355481c1acc360a60d2d55d68
Jineshbansal <732005jinesh@gmail.com>
2024-12-11 18:47:17 +0530

    Completed fibo functions

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 fn main() {
     println!("Hello, world!");
-    println!("{}",is_even(2));
-
+    let num: i32= 5;
+    let result: i32 = fibo(num);
+    println!("Fibonacci of {} is {}", num, result);
 }
-// General practice in rust to write in snake case
-fn is_even(n:i32)->bool{
-    if  n%2==0 {
-        return true;
+
+fn fibo(num:i32)->i32{
+    let mut first=0;
+    let mut second=1;
+    if num==0{
+        return first;
     }
-    else{
-        return false;
+    if num==1{
+        return second;
     }
+    for _ in 1..num{
+        let temp=second;
+        second=first+second;
+        first=temp;
+    }
+    return second;
 }
