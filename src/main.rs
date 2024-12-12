989b891a42c07f14edc5464776f94334c300122c
Jineshbansal <732005jinesh@gmail.com>
2024-12-12 14:18:06 +0530

    hashmap created

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
-use std::vec;
-
-
+use std::collections::HashMap;
 fn main() {
-    let num=vec![1,2,3,4,5,6,7,8,9,10];
-    let even_values=get_even_values(num);
-    println!("Even values are {:?}",even_values);
+    let mut mapp=HashMap::new();
+    mapp.insert("Jinesh",20);
+    mapp.insert("Vijay",40);
+    println!("{:?}",mapp["Jineshe"]);
+
 }
 
-fn get_even_values(num:Vec<i32>)->Vec<i32>{
-    let mut even_values=Vec::new();
-    for i in num{
-        if i%2==0{
-            even_values.push(i);
-        }
-    }
-    even_values
-}
- 
    
