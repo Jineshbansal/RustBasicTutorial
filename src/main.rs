5bc877fc04cd8c42730270e0711cb463d8d12cd2
Jineshbansal <732005jinesh@gmail.com>
2024-12-12 16:28:29 +0530

    Iterators first half completed

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,44 @@
-use std::collections::HashMap;
+ 
 fn main() {
-    let mut mapp=HashMap::new();
-    mapp.insert("Jinesh",20);
-    mapp.insert("Vijay",40);
-    println!("{:?}",mapp["Jineshe"]);
+    let v1=vec![1,2,3,4,5];
+    // to borrow it mutably used iter_mut 
+    let v1_iter=v1.iter();
+    let total:i32=v1_iter.sum();
+    println!("Sum: {}",total);
+    println!("v1: {:?}",v1);
+    /*  It is same as 
+    for val in v1{
+        println!("Got: {}",val);
+    }   
+    In iter methods work by borrowing
+    We can't mutate variables as refernce passed is immutable 
+    */
+     
+    // For loop uses into_iter by default
+    // for val in v1_iter{
+    //     println!("Got: {}",val);
+    // }
+    /*
+    Another way to iterate is to use into_iter
+    let mut iter=v1.iter();
+    while let Some(val)=iter.next(){
+        println!("Got: {}",val);
+    }
+    
+     */
+    
+    /*
+    into_iter take ownership of the collection
+     */
+
+    /*
+    Consuming adapters and iterator adaptors
+    let v1_iter=v1.iter();
+    let total=v1_iter.sum();
+    you can't use v1_iter after this as it has been consumed
+    
+    Iterator adaptors  don't consume iterator.Instead they produce different iterators by changing some aspect of the original  iterator.
+     */
 
 }
 
