d141241c95db5a8db93a96c41539a5ce781b4b9c
Jineshbansal <732005jinesh@gmail.com>
2024-12-12 16:39:51 +0530

    Iterators completed

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,5 @@
  
 fn main() {
-    let v1=vec![1,2,3,4,5];
-    // to borrow it mutably used iter_mut 
-    let v1_iter=v1.iter();
-    let total:i32=v1_iter.sum();
-    println!("Sum: {}",total);
-    println!("v1: {:?}",v1);
-    /*  It is same as 
-    for val in v1{
-        println!("Got: {}",val);
-    }   
-    In iter methods work by borrowing
-    We can't mutate variables as refernce passed is immutable 
-    */
-     
-    // For loop uses into_iter by default
-    // for val in v1_iter{
-    //     println!("Got: {}",val);
-    // }
-    /*
-    Another way to iterate is to use into_iter
-    let mut iter=v1.iter();
-    while let Some(val)=iter.next(){
-        println!("Got: {}",val);
-    }
-    
-     */
-    
-    /*
-    into_iter take ownership of the collection
-     */
-
     /*
     Consuming adapters and iterator adaptors
     let v1_iter=v1.iter();
@@ -38,7 +7,18 @@ fn main() {
     you can't use v1_iter after this as it has been consumed
     
     Iterator adaptors  don't consume iterator.Instead they produce different iterators by changing some aspect of the original  iterator.
-     */
+     */  
+
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+    let v2_iter = v1_iter.map(|x| x + 1);
+    let v3_iter = v2_iter.filter(|x| * x==2);
+    for i in v3_iter {
+        println!("{}", i);
+    } 
+    // converting iterator to vector
+    let v: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    println!("{:?}", v);
 
 }
 
