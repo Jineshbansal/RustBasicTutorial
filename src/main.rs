b1e5e7356d3546a2242fa0c04d76958616865f73
Jineshbansal <732005jinesh@gmail.com>
2024-12-12 14:02:50 +0530

    vector completed

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
+use std::vec;
+
 
 fn main() {
-    let mut s1=String::from("hello");
-    let mut s2=&s1; 
-    // let s3=&mut s1;
-    // println!("{}",s);
+    let num=vec![1,2,3,4,5,6,7,8,9,10];
+    let even_values=get_even_values(num);
+    println!("Even values are {:?}",even_values);
+}
+
+fn get_even_values(num:Vec<i32>)->Vec<i32>{
+    let mut even_values=Vec::new();
+    for i in num{
+        if i%2==0{
+            even_values.push(i);
+        }
+    }
+    even_values
 }
  
    
