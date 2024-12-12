f5be480735d66471e0334b18d311ac1e5e9f4025
Jineshbansal <732005jinesh@gmail.com>
2024-12-12 23:06:57 +0530

    Strings completed

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,12 @@
  
 fn main() {
-    /*
-    Consuming adapters and iterator adaptors
-    let v1_iter=v1.iter();
-    let total=v1_iter.sum();
-    you can't use v1_iter after this as it has been consumed
-    
-    Iterator adaptors  don't consume iterator.Instead they produce different iterators by changing some aspect of the original  iterator.
-     */  
+     /*
+     slice let you reference a contiguous sequence of elements in a collection rather than the whole collection.
+     A slice is a kind of reference, so it doesn’t have ownership. 
 
-    let v1 = vec![1, 2, 3];
-    let v1_iter = v1.iter();
-    let v2_iter = v1_iter.map(|x| x + 1);
-    let v3_iter = v2_iter.filter(|x| * x==2);
-    for i in v3_iter {
-        println!("{}", i);
-    } 
-    // converting iterator to vector
-    let v: Vec<_> = v1.iter().map(|x| x + 1).collect();
-    println!("{:?}", v);
+    */
+    //slice can also be applied to other collection like vectors/arrays  
+    let string_literal = "Hello, world!";// literal is also an &str type but it points directly to the address of the string in the binary
 
 }
 
