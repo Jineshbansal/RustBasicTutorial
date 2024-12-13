e046a5bb564b97f5d412ada51a55840d5fd4016b
Jineshbansal <732005jinesh@gmail.com>
2024-12-13 12:12:05 +0530

    multithreading example

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-struct User<'a>{
-    name:&'a str,
-}
+use std::thread;
+use std::time::Duration;
 
+fn main() {    
 
-
-fn main() {   
-    let name=String::from("John");
-    let user=User{
-        name :&name,
-    };
-    println!("{}",user.name);
-
+    let handle=thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    for i in 1..100 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+    handle.join().unwrap();
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
 
 }
 
