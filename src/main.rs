d56682e223ff3ccdb4bf15a643caf60d644890c8
Jineshbansal <732005jinesh@gmail.com>
2024-12-13 12:29:56 +0530

    basic channel example

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use std::thread;
 
-fn main() {    
-
-    let v=vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    thread::spawn(move ||{
-        for i in v.iter(){
-            println!("Thread 1: {}", i);
-        }
+fn main() {     
+    let (tx, rx) = std::sync::mpsc::channel();   
+    let handle = thread::spawn(move || { 
+        for i in 0..10000{
+            println!("{}",i);
+        }        
+        tx.send("Hello from the thread!").unwrap();     
     });
+    println!("{}", rx.recv().unwrap());
+    /* 
+    you should not use unwrap as it will panic if the result is an error. 
+    */
+    let value=rx.recv();
+    match value{
+        Ok(value)=>println!("{}",value),
+        Err(e)=>println!("Error: {}",e),
+    }
 
 } 
 
