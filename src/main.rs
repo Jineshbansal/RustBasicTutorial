7eef26ba2f44b7e827ca9d3b71a7b0eb72078753
Jineshbansal <732005jinesh@gmail.com>
2024-12-13 11:54:48 +0530

    structs with lifetime

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,15 @@
-// Shorter lifetime is what the return type will have
-
-
-fn longer_str<'a>(s1:&'a str, s2:&'a str) -> &'a str {
-    if s1.len() > s2.len() {
-        s1
-    } else {
-        s2
-    } 
+struct User<'a>{
+    name:&'a str,
 }
 
 
-fn main() { 
-    let longest_str;
-    let s1 = String::from("small");
-    {
-        let s2 = String::from("longer");
-        longest_str = longer_str(&s1, &s2);
-    }
-    println!("The longest string is: {}", longest_str); 
-    
-    
+
+fn main() {   
+    let name=String::from("John");
+    let user=User{
+        name :&name,
+    };
+    println!("{}",user.name);
 
 
 }
